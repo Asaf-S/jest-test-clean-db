89d956b00d5b186fe7866826dc37e49d1f57c905
Asaf <AsafShifer@Gmail.com>
2025-06-15 15:13:47 +0300

    .

--- app/globalSetup.js
+++ app/globalSetup.js
@@ -1,3 +1,3 @@
-const globalSetup = require('../dist/globalSetup');
+const globalSetup = require('./dist/globalSetup');
 exports.default = globalSetup.default;
 module.exports = globalSetup.default;
